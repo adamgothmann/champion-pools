00fa3fad6ca67fcf3796546bb903dadca568d3b3
adamgothmann <adamgothmann@gmail.com>
2017-01-03 11:49:02 -0600

    names showing up on dom

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,22 +2,21 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
   var key;
-  $scope.apiHit = function(){
+  $scope.getKey = function(){
     $http({
       method: 'GET',
       url: '/key'
     }).then(function(response){
       key = response.data;
       console.log(key);
-    });
-    $http({
-      method: 'GET',
-      url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=' + key + ''
-    }).then(function(response){
-      $scope.champions = response.data.data;
-      console.log($scope.champions);
-      console.log($scope.champions.length);
+      $http({
+        method: 'GET',
+        url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=' + key + ''
+      }).then(function(response){
+        $scope.champions = response.data.data;
+        console.log($scope.champions);
+      });
     });
   };
-  $scope.apiHit();
+  $scope.getKey();
 }]);
